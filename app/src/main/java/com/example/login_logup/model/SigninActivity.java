7ab1626743f50c9e27adeb4634095276d0fedd3d
Nguyenpham12 <nguyenpham6273@gmail.com>
2024-02-14 19:26:46 +0700

    display

---
+++ app/src/main/java/com/example/login_logup/model/SigninActivity.java
@@ -0,0 +1,12 @@
+package com.example.login_logup;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SigninActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signin);
+    }
+}
